666aeccda5c9e770bb7ce8d0523200b818a654ae
Fiooona <845559655@qq.com>
2016-05-18 17:44:37 +0800

    finish

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,14 @@
 function calculate_median(arr) {
-  //请勿改动函数名
+    for(var n= 0,i=0;i<arr.length;i++){
+        if((i%2)!=0){
+            //奇数位
+            var num;
+            num+=arr[i];
+            n++;
+        }
+        var median_num=num/n;
+    }
+    return   median_num
 }
 
 module.exports = calculate_median;
