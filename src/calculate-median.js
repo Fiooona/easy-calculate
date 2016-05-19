4b04d0928315019489e67ebdf478a8c428af7a0a
Fiooona <845559655@qq.com>
2016-05-19 09:47:29 +0800

    god

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,7 +1,8 @@
-var n=0;
-var index=0;
+
+
 function calculate_median(arr) {
-    for(var i=0;i<arr.length;i++){
+    var index=0;
+    for(var n= 0,i=0;i<arr.length;i++){
         if((i%2)!=0){
             //偶数位n个
             n++;
@@ -10,15 +11,14 @@ function calculate_median(arr) {
     if ((n%2)==0){
         //偶数
         index=n;
-        return ((arr[index-1]+arr[index+1])/2);
+        var num=( arr[index-1]+arr[index+1])/2 ;
     }
     else{
         index=n;
-        return   arr[index];
+        num= arr[index];
     }
-
+    return num
 }
-
 //arr=[1,2,3,4,5,6,7,8,9];
 //console.log(calculate_median(arr));
 module.exports = calculate_median;
